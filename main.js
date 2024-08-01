ec5be7d5b11616e8f73d6502accc48a39cb7b077
suyashsharma12 <mehusuyash@gmail.com>
2024-08-01 14:50:05 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings:["Full-Stack Developer","Web Designer","Data analyst"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+    
+});
